4059262c2a93430534cabfb759d373b81834fc4b
Domenic Betters <108141041+domenicbetters@users.noreply.github.com>
2023-11-20 16:12:36 -0500

    added redirects for all urls

--- src/router/index.js
+++ src/router/index.js
@@ -29,9 +29,17 @@ const routes = [
         component: LocationPage
     },
     {
+      path: '/location',
+      redirect: { name: 'Location' }
+    },
+    {
         path: "/AntoniosPizza/menu/full",
         name: "Full Menu",
         component: MenuPage
+    },
+    {
+      path: '/menu/full',
+      redirect: { name: 'Full Menu' }
     },
     {
         path: "/AntoniosPizza/menu/vegan",
@@ -39,9 +47,17 @@ const routes = [
         component: MenuPage
     },
     {
+      path: '/menu/vegan',
+      redirect: { name: 'Vegan Menu' }
+    },
+    {
         path: "/AntoniosPizza/vegan",
         name: "Vegan",
         component: VeganPage
+    },
+    {
+      path: '/vegan',
+      redirect: { name: 'Vegan' }
     },
     {
         path: "/AntoniosPizza/catering",
@@ -49,19 +65,36 @@ const routes = [
         component: CateringPage
     },
     {
+      path: '/catering',
+      redirect: { name: 'Catering' }
+    },
+    {
         path: "/AntoniosPizza/events",
         name: "Events",
         component: EventPage
+    },
+    {
+      path: '/events',
+      redirect: { name: 'Events' }
     },
     {
         path: "/AntoniosPizza/about",
         name: "About",
         component: AboutPage
     },
+    
+    {
+        path: '/about',
+        redirect: { name: 'About' }
+      },
     {
         path: "/AntoniosPizza/contact",
         name: "Contact",
         component: ContactPage
+    },
+    {
+      path: '/contact',
+      redirect: { name: 'Contact' }
     },
       
   ]
